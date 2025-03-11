54eca4bd8c59d0f96073d273badd79e432e44745
Kumar1502 <68464602+Kumar1502@users.noreply.github.com>
2025-03-11 20:11:00 +0800

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "myproject-march11"
+    bucket         = "myproject-aug21"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "dynamo-terraform"
+    dynamodb_table = "dynamo-terra"
     encrypt        = true
   }
 }
